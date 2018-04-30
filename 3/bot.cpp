9170df4f3749ab65868ba2c46478ef0f4e0b173d
Benjamin Prud'homme <bgprudhomme@gmail.com>
2018-04-30 16:29:06 -0400

    Prevent repairing robots from collecting, update coordinate variables when moving

--- 3/bot.cpp
+++ 3/bot.cpp
@@ -1,3 +1,15 @@
+/*
+bot.cpp
+Author: Benjamin Prud'homme
+Course: CSCI-135
+Assignment: Project 3
+
+A program involving space robots collecting debris.  This file
+contains the logic for the robot's actions, including collecting debris,
+searching for and moving towards nearby debris, and repairing neighboring
+robots that have failed.
+*/
+
 #include <cstdlib>
 #include <climits>
 #include <cmath>
@@ -46,12 +58,6 @@ void onStart(int num, int rows, int cols, double mpr,
 	log << "Start" << endl;
 }
 
-void indexInRange(int index, int arraySize, ostream &log){
-	if(index<0 || index>arraySize){
-		log << "Error: attempting to reference index " << index << " of an array of size " << arraySize << endl;
-	}
-}
-
 bool cellOpen(int row, int col){
 	if(((row<0) || (row>=ROWS)) || ((col<0) || (col>=COLS))){
 		return false;
@@ -68,7 +74,6 @@ bool cellOpen(int row, int col){
 
 /* Have robot with given id move towards the passed-in cell */
 Action moveTowardsCell(int robotID, int cellRow, int cellCol, ostream &log){
-	indexInRange(robotID, NUM, log); // debugging
 	int row = robotRows[robotID];
 	int col = robotCols[robotID];
 
@@ -102,6 +107,7 @@ Action moveTowardsCell(int robotID, int cellRow, int cellCol, ostream &log){
 				case 0:
 					if(canMoveLeft){
 						log << "Bot " << robotID << ": Moving left\n";
+						robotCols[robotID]--;	
 						return LEFT;
 					}
 					log << "Bot " << robotID << ": Can't move left\n";
@@ -109,6 +115,7 @@ Action moveTowardsCell(int robotID, int cellRow, int cellCol, ostream &log){
 				case 1:
 					if(canMoveRight){
 						log << "Bot " << robotID << ": Moving right\n";
+						robotCols[robotID]++;	
 						return RIGHT;
 					}
 					log << "Bot " << robotID << ": Can't move right\n";
@@ -116,6 +123,7 @@ Action moveTowardsCell(int robotID, int cellRow, int cellCol, ostream &log){
 				case 2:
 					if(canMoveUp){
 						log << "Bot " << robotID << ": Moving up\n";
+						robotRows[robotID]--;	
 						return UP;
 					}
 					log << "Bot " << robotID << ": Can't move up\n";
@@ -123,6 +131,7 @@ Action moveTowardsCell(int robotID, int cellRow, int cellCol, ostream &log){
 				default:
 					if(canMoveDown){
 						log << "Bot " << robotID << ": Moving down\n";
+						robotRows[robotID]++;	
 						return DOWN;
 					}
 					log << "Bot " << robotID << ": Can't move down\n";
@@ -135,15 +144,19 @@ Action moveTowardsCell(int robotID, int cellRow, int cellCol, ostream &log){
 
 	// If we cannot get closer to the target, move randomly in one of the other directions
 	if(canMoveLeft){
+		robotCols[robotID]--;
 		return LEFT;
 	}
 	else if(canMoveRight){
+		robotCols[robotID]++;
 		return RIGHT;
 	}
 	else if(canMoveUp){
+		robotRows[robotID]--;
 		return UP;
 	}
 	else {
+		robotRows[robotID]++;
 		return DOWN;
 	}
 	
@@ -153,7 +166,6 @@ Action moveTowardsCell(int robotID, int cellRow, int cellCol, ostream &log){
 /* Returns the ID of the robot targeting a debris field at the given coordinates (other than the one with the given ID). */
 int otherRobotTargetingDebris(int robotID, int debrisRow, int debrisCol, ostream &log){
 	for(int i=0; i<NUM; i++){
-		indexInRange(i, NUM, log); // debugging
 		if(i!=robotID && ((targetRows[i] == debrisRow) && (targetCols[i] == debrisCol))){
 			log << "Bot " << robotID << ": (" << debrisRow << ", " << debrisCol << ") is already being targeted by bot " << i << endl;
 			return i;
@@ -166,7 +178,6 @@ int otherRobotTargetingDebris(int robotID, int debrisRow, int debrisCol, ostream
 
 /* Deciding robot's next move */
 Action onRobotAction(int id, Loc loc, Area &area, ostream &log) {
-	indexInRange(id, NUM, log);
 	int row = loc.r; // current row and column
 	int col = loc.c;
 
@@ -175,9 +186,10 @@ Action onRobotAction(int id, Loc loc, Area &area, ostream &log) {
 	robotRows[id] = row;
 	robotCols[id] = col;
 
-	// if the robot is in a debris field, collect it and clear this robot's target variables (unless
-	// this robot has been sent on a repair mission)
-	if (area.inspect(robotRows[id], robotCols[id]) == DEBRIS){
+	// if the robot is in a debris field, collect it and clear this robot's target variables, unless
+	// this robot has been sent on a repair mission.  This reduces the chance that a robot assigned to
+	// repair a certain robot will malfunction on the way to repairing, which could create chaos.
+	if ((area.inspect(robotRows[id], robotCols[id]) == DEBRIS) && !repairing[id]){
 
 		// if this robot has been sent to repair, it will continue towards the malfunctioned
 		// robot after it collects  This allows it to collect debris on its way to repairing.
@@ -199,7 +211,6 @@ Action onRobotAction(int id, Loc loc, Area &area, ostream &log) {
 		// repair it and assign the repaired robot to collect the debris in its cell
 		if(repairing[id] == true){
 			for(int i=0; i<NUM; i++){
-				indexInRange(i, NUM, log); // debugging
 				if(malfunctioned[i]){
 					if((row+1==robotRows[i]) && (col==robotCols[i])){
 						repairing[id] = false;
@@ -295,11 +306,8 @@ Action onRobotAction(int id, Loc loc, Area &area, ostream &log) {
 
 						// otherwise, determine if this robot is closer than the other
 						else {
-							log << "which is already being targeted by bot " << otherRobotID << endl;
-
 							// if this other robot is further away from the debris than this robot, this robot will take over the other's duty.
-							// if the other robot is equidistant from or closer to the debris, this robot will ignore the debris
-							indexInRange(otherRobotID, 50, log); // debugging
+							// if the other robot is equidistant from or closer to the debris, this robot will ignore it
 							if(abs(robotRows[otherRobotID]-currentRow)+abs(robotCols[otherRobotID]-(col+(movesAway-rowsFromBot)))>movesAway){
 								log << "Bot " << otherRobotID << ": No longer targeting (" << targetRows[otherRobotID] << ", " << targetCols[otherRobotID] << ")\n";
 								
@@ -325,8 +333,6 @@ Action onRobotAction(int id, Loc loc, Area &area, ostream &log) {
 						int otherRobotID = otherRobotTargetingDebris(id, currentRow, col-(movesAway-rowsFromBot), log);
 
 						if(otherRobotID==-1){
-							log << "No other bots are targeting this debris\n";
-
 							targetRows[id] = currentRow;
 							targetCols[id] = col-(movesAway-rowsFromBot);
 
@@ -336,11 +342,8 @@ Action onRobotAction(int id, Loc loc, Area &area, ostream &log) {
 
 						// otherwise, determine if this robot is closer than the other
 						else {
-							log << "which is already being targeted by bot " << otherRobotID << endl;
-
 							// if this other robot is further away from the debris than this robot, this robot will take over the other's duty.
 							// if the other robot is equidistant from or closer to the debris, this robot will ignore the debris
-							indexInRange(otherRobotID, 50, log); // debugging
 							if(abs(robotRows[otherRobotID]-currentRow)+abs(robotCols[otherRobotID]-(col-(movesAway-rowsFromBot)))>movesAway){
 								log << "Bot " << otherRobotID << ": No longer targeting (" << targetRows[otherRobotID] << ", " << targetCols[otherRobotID] << ")\n";
 								
@@ -379,13 +382,13 @@ Action onRobotAction(int id, Loc loc, Area &area, ostream &log) {
 
 		}
 
+		// if the robot already has a target, move towards it
 		return moveTowardsCell(id, targetRows[id], targetCols[id], log);
 		
 	}
 }
 
 void onRobotMalfunction(int id, Loc loc, Area &area, ostream &log) {
-	indexInRange(id, NUM, log);
 
 	// if a robot malfunctions, send the closest functional robot to repair it
 	log << "Bot " << id << " is damaged." << endl;
@@ -399,7 +402,6 @@ void onRobotMalfunction(int id, Loc loc, Area &area, ostream &log) {
 
 	// find the closest functional robot
 	for(int i=0; i<NUM; i++){
-		indexInRange(i, NUM, log); // debugging
 		if(!malfunctioned[i] && (abs(robotRows[i]-loc.r)+abs(robotCols[i]-loc.c)<closestRobotDistance)){
 			closestRobotID = i;
 			closestRobotDistance = abs(robotRows[i]-loc.r)+abs(robotCols[i]-loc.c);
@@ -407,8 +409,6 @@ void onRobotMalfunction(int id, Loc loc, Area &area, ostream &log) {
 	}
 
 	log << "Bot " << closestRobotID << " has been assigned to repair it.\n";
-
-	indexInRange(closestRobotID, NUM, log); // debugging
 
 	targetRows[closestRobotID] = loc.r;
 	targetCols[closestRobotID] = loc.c;
